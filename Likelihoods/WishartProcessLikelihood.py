ca911a138ced316accda5c4d18a49ea9d65b619b
David <leeft047@planet.nl>
2021-02-10 13:31:13 +0100

    initial Inv Wishart Process likelihood and Inv Wishart Process model

---
+++ Likelihoods/WishartProcessLikelihood.py
@@ -0,0 +1,28 @@
+import numpy as np
+import gpflow
+import tensorflow as tf
+from tensorflow_probability import distributions as tfd
+
+class InvWishartProcessLikelihood(gpflow.likelihoods.Likelihood):
+    def __init__(self, D, DoF, R=1):
+        # To do: confirm if latent dim is correctly specified. gpflow 1 did not require this
+        super().__init__(latent_dim=D*DoF, observation_dim=D)
+        self.D = D
+        self.DoF = DoF
+        self.R = R # n_samples for variational expectations for automatic differentiation
+
+        # Note: gpflow transforms -> replaced by tensorflow_probability.bijectors
+
+    def variational_expectations(self, mu, S, Y):
+        """
+        Function written by Creighton Heaukulani and Mark van der Wilk, adapted for tensorflow 2.4
+        """
+        N, D = tf.shape(Y)
+        W = tf.random.normal([self.R, N, tf.shape(mu)[1]])
+        F = W*(S**0.5) + mu  # samples through which TF automatically differentiates
+        # compute the (mean of the) likelihood
+        AF = self.A_diag[:, None] * tf.reshape(F, [self.R, N, D, -1])
+        yffy = tf.reduce_sum(tf.einsum('jk, ijkl− > ijl', Y, AF)** 2.0, axis=-1)
+        chols = tf.linalg.cholesky(tf.matmul(AF, AF, transpose_b=True))  # cholesky of precision logp = tf.reduce_sum(tf.log(tf.matrix_diag_part(chols)), axis=2) − 0.5 ∗ yffy return tf .reduce_mean(logp , axis=0)
+        logp = tf.reduce_sum(tf.math.log(tf.linalg.diag_part(chols)), axis=2) - 0.5 * yffy
+        return tf.reduce_mean(logp, axis=0)
---
+++ Models/WishartProcess.py
@@ -0,0 +1,23 @@
+import numpy as np
+import gpflow
+import tensorflow as tf
+from tensorflow_probability import distributions as tfd
+from Likelihoods.WishartProcessLikelihood import InvWishartProcessLikelihood
+
+
+class InvWishartProcess(gpflow.models.SVGP):
+    # Upgrade guide SVGP Model:
+    # https://gpflow.readthedocs.io/en/master/notebooks/gpflow2_upgrade_guide.html#SVGP-Initialiser
+    # Short summary: - X and Y should be passed to elbo()/training_loss()
+    #                - Z is no longer accepted -> inducing points
+    #                - feature parameter renamed to inducing points as well
+    #                - minibatch size is done by elbo(): data shape should be in batches to do this.
+
+    def __init__(self, kernel, likelihood=None, D=1, DoF=None, inducing_variable=None):
+        DoF = D + 1 if DoF is None else DoF
+        likelihood = InvWishartProcessLikelihood(D, DoF, R=10) if likelihood is None else likelihood
+
+        super().__init__(kernel=kernel,
+                         likelihood=likelihood,
+                         inducing_variable=inducing_variable,
+                         num_latent_gps=D*DoF)
