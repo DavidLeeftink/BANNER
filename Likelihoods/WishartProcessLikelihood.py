4fcf25525fc24460fb63147af678a8450e4797ad
David <leeft047@planet.nl>
2021-03-03 13:36:44 +0100

    finished (largely untested) full wishart likelihood class

--- Likelihoods/WishartProcessLikelihood.py
+++ Likelihoods/WishartProcessLikelihood.py
@@ -1,8 +1,10 @@
 import numpy as np
 import gpflow
 import tensorflow as tf
-from tensorflow_probability import distributions as tfd
+import tensorflow_probability as tfp
 from gpflow.likelihoods.base import ScalarLikelihood
+from gpflow.utilities import positive
+from gpflow import Parameter
 
 
 class WishartLikelihoodBase(ScalarLikelihood):
@@ -11,7 +13,6 @@ class WishartLikelihoodBase(ScalarLikelihood):
     """
     def __init__(self, D, DoF, R=10, model_inverse=True, additive_noise=True, **kwargs):
         # Todo: confirm if latent dim is correctly specified. gpflow 1 did not require this
-        # Note: gpflow transforms -> replaced by tensorflow_probability.bijectors
         """
         :param D (int) Covariance matrix dimension
         :param DoF (int) Degrees of freedom
@@ -90,10 +91,81 @@ class WishartLikelihoodBase(ScalarLikelihood):
         """
         raise NotImplementedError
 
-class FullCovLikelihood(WishartLikelihoodBase):
+class FullWishartLikelihood(WishartLikelihoodBase):
     """
     Concrete class for the full covariance likelihood models.
+    The code is written by Heaukulani-van der Wilk (see references above)
     """
-    def __init__(self, D, DoF, **kwargs):
-        assert DoF>=D, "Degrees of freedom must be larger or equal than the number of dimensionality of the covariance matrix"
+    def __init__(self, D, DoF, A=None, **kwargs):
+        """
+        :param D (int) Dimensionality of covariance matrix
+        :param DoF (int) degrees of freedom
+        :param A (DxD matrix) scale matrix. Default is a DxD identity matrix.
+        """
+        assert DoF>=D, "Degrees of freedom must be larger or equal than the dimensionality of the covariance matrix"
         super().__init__(D, DoF, **kwargs)
+
+        # this case assumes a square scale matrix, and it must lead with dimension D
+        self.A = A if A is not None else Parameter(np.ones(self.D), transform=positive(), dtype=tf.float32)
+
+        if self.additive_noise:
+            # create additional noise param; should be positive; conc=0.1 and rate=0.0001 initializes sigma2inv=1000 and thus initializes sigma2=0.001
+            self.p_sigma2inv_conc = Parameter(0.1, transform=positive(), dtype=tf.float32)
+            self.p_sigma2inv_rate = Parameter(0.0001, transform=positive(), dtype=tf.float32)
+            self.q_sigma2inv_conc = Parameter(0.1 * np.ones(self.D), transform=positive(), dtype=tf.float32)
+            self.q_sigma2inv_rate = Parameter(0.0001 * np.ones(self.D), transform=positive(), dtype=tf.float32)
+
+    def make_gaussian_components(self, F, Y):
+        """
+        An auxiliary function for logp that returns the complexity pentalty and the data fit term.
+        Note: it is assumed that the mean function is 0.
+        :param F: (R, N, D, __) - the (samples of the) matrix of GP outputs, where:
+                R is the number of Monte Carlo samples,
+                N is the numkber of observations and
+                D the dimensionality of the covariance matrix.
+        :param Y: (N, D) observations
+        :return:
+            log_det_cov: (R,N) log determinant of the covariance matrix Sigma_n (complexity penalty)
+            yt_inv_y: (R,N) (data fit term)
+
+        """
+        ## Compute Sigma_n (aka AFFA)
+        AF = self.A[:, None] * F  # (R, N, D, DoF)
+        AFFA = tf.matmul(AF, AF, transpose_b=True)  # (R, N, D, D)
+
+        ## additive white noise (Lambda) for numerical precision
+        if self.additive_noise:
+            n_samples = tf.shape(F)[0]  # could be 1 if making predictions
+            dist = tfp.distributions.Gamma(self.q_sigma2inv_conc, self.q_sigma2inv_rate)## todo: why does pycharm not access Gamma()?
+
+            sigma2_inv = dist.sample([n_samples])  # (R, D)
+            sigma2_inv = tf.clip_by_value(sigma2_inv, 1e-8, np.inf)
+
+            if self.model_inverse:
+                Lambda = sigma2_inv[:, None, :]
+            else:
+                sigma2 = np.power(sigma2_inv, -1.0)
+                Lambda = sigma2[:, None, :]
+        else:
+            Lambda = 1e-5
+
+        ## Compute log determinant of covariance matrix Sigma_n (aka AFFA)
+        AFFA = tf.linalg.set_diag(AFFA, tf.linalg.diag_part(AFFA) + Lambda)
+        L = tf.linalg.cholesky(AFFA)  # (S, N, D, D)
+        log_det_cov = 2 * tf.reduce_sum(tf.math.log(tf.linalg.diag_part(L)), axis=2)  # (S, N)
+        if self.model_inverse:
+            log_det_cov = - log_det_cov
+        Y.set_shape([None, self.D])
+
+        ## Compute (Y^T affa^inv Y) term
+        if self.model_inverse:
+            y_prec = tf.einsum('jk,ijkl->ijl', Y, AFFA)  # (S, N, D)
+            yt_inv_y = tf.reduce_sum(y_prec * Y, axis=2)  # (S, N)
+
+        else:
+            n_samples = tf.shape(F)[0]  # could be 1 when computing MAP test metric
+            Ys = tf.tile(Y[None, :, :, None], [n_samples, 1, 1, 1])  # this is inefficient, but can't get the shapes to play well with cholesky_solve otherwise
+            L_solve_y = tf.linalg.triangular_solve(L, Ys, lower=True)  # (S, N, D, 1)
+            yt_inv_y = tf.reduce_sum(np.square(L_solve_y, 2), axis=(2, 3))  # (S, N)
+
+        return log_det_cov, yt_inv_y
--- Models/WishartProcess.py
+++ Models/WishartProcess.py
@@ -2,7 +2,7 @@ import numpy as np
 import gpflow
 import tensorflow as tf
 from tensorflow_probability import distributions as tfd
-from Likelihoods.WishartProcessLikelihood import WishartLikelihoodBase
+from Likelihoods.WishartProcessLikelihood import WishartLikelihoodBase, FullWishartLikelihood
 
 
 class InvWishartProcess(gpflow.models.SVGP):
@@ -16,7 +16,7 @@ class InvWishartProcess(gpflow.models.SVGP):
     def __init__(self, kernel, likelihood=None, D=1, DoF=None, inducing_variable=None):
         # Todo: not clear how the latent GPs work. Currently works only with 1 degree of freedom.
         DoF = D if DoF is None else DoF
-        likelihood = InvWishartProcessLikelihood(D, DoF, R=10) if likelihood is None else likelihood
+        likelihood = FullWishartLikelihood(D, DoF, R=10) if likelihood is None else likelihood
 
         super().__init__(kernel=kernel,
                          likelihood=likelihood,
