44a22715b668d3103b9c9ed40163f8496910fd0b
David <leeft047@planet.nl>
2021-04-17 15:58:24 +0200

    removed n-longer-used analyses files

--- Analyses/Demo.py
+++ Analyses/Demo.py
@@ -1,33 +1,77 @@
-# %%
 from Likelihoods import WishartProcessLikelihood
 from Models import WishartProcess
+from Models.training_util import *
 import tensorflow as tf
 import gpflow
 from gpflow.utilities import print_summary
-from gpflow.kernels import Sum, Cosine, SquaredExponential
+from gpflow.kernels import Sum, Cosine, SquaredExponential, Periodic, Linear, SharedIndependent
+from gpflow.inducing_variables import SharedIndependentInducingVariables, InducingPoints
+from gpflow.ci_utils import ci_niter
 import numpy as np
 from numpy.random import uniform, normal
 from scipy.stats import norm
+import matplotlib.pyplot as plt
 np.random.seed(2022)
 tf.random.set_seed(2021)
-import matplotlib.pyplot as plt
 
-### Generate synthetic data & visualize results
+################################################
+#####  Create synthetic data from GP prior #####
+################################################
+
+## data properties
 T = 10
-N = 500  # time points
-D = 4
-true_mean = np.zeros(D)
-true_cov = np.array([[.1,0,0.8,0],[0,.1,0,0.9],[0.8,0,.1,0],[0,0.9,0,.1]])
+N = 100
+D = 3
+X = np.array([np.linspace(0, T, N) for i in range(D)]).T # input time points
+true_lengthscale = 2.5
 
-X = np.array([np.linspace(0, T, N) for i in range(D)]).T  # input time points
-Y = np.random.multivariate_normal(true_mean, true_cov, N)
-#Y = Y + 0.2*np.cos(0.6 * np.ones(D)) + 0.3*np.sin(1.1 * X *np.ones(D))
-data = (X, Y)
+## Model properties
+model_inverse = False
+additive_noise = True
+DoF = D+1  # Degrees of freedom
+latent_dim = int(DoF * D)
+R = 10  # samples for variational expectation
+M = 20  # num inducing point. exact (non-sparse) model is obtained by setting M=N
+shared_kernel = True  # shares the same kernel parameters across input dimension if true
 
-fig, ax = plt.subplots(1, 1)
-ax.imshow(true_cov, cmap='Reds')
-ax.set_title(f'True covariance')
+if M == N:
+    Z_init = tf.identity(X)  # X.copy()
+else:
+    Z_init = np.array([np.linspace(0, T, M) for i in range(D)]).T  # .reshape(M,1) # initial inducing variable locations
+Z = tf.identity(Z_init)
+iv = SharedIndependentInducingVariables(InducingPoints(Z))  # multi output inducing variables
+
+## create GP model for the prior
+kernel_prior = SquaredExponential(lengthscales=true_lengthscale)
+#kernel_prior = Sum([SquaredExponential() * Cosine(lengthscales=2. / (i + 1)) for i in range(2)])
+kernel_prior = SharedIndependent(kernel_prior,output_dim=latent_dim)
+likelihood_prior = WishartProcessLikelihood.FullWishartLikelihood(D, DoF, R=R,
+                                                            additive_noise=additive_noise,
+                                                            model_inverse=model_inverse)
+wishart_process_prior = WishartProcess.FullCovarianceWishartProcess(kernel_prior, likelihood_prior, D=D, DoF=DoF, inducing_variable=iv)
+f_sample = wishart_process_prior.predict_f_samples(X, 1)
+A = np.identity(D)
+f_sample = tf.reshape(f_sample, [N, D, -1]) # (n_samples, D, DoF)
+Sigma_gt = np.matmul(f_sample, np.transpose(f_sample, [0, 2, 1]))
+
+
+
+fig, ax = plt.subplots(D,D,figsize=(10,10))
+for i in range(D):
+    for j in range(D):
+        if i <= j:
+            ax[i,j].set_title(r'$\Sigma_{{{:d}{:d}}}$'.format(i, j))
+            ax[i,j].plot(X, Sigma_gt[:,i,j], color='C0',label='True function')
+        else:
+            ax[i, j].axis('off')
 plt.show()
+
+## create data by sampling from mvn at every timepoint
+Y = np.zeros((N, D))
+for t in range(N):
+    Y[t, :] = np.random.multivariate_normal(mean=np.zeros((D)), cov=Sigma_gt[t, :, :])
+
+data = (X, Y)
 
 fig, ax = plt.subplots(D, 1, sharex=True, sharey=True, figsize=(10, 6))
 if not isinstance(ax, np.ndarray):
@@ -42,90 +86,87 @@ for i in range(D):
         ax[i].set_xlabel('time')
 plt.show()
 
-### Model initialization
-# Model/training parameters
-DoF = D  # Degrees of freedom
-latent_dim = int(DoF * D)
-R = 10  # samples for variational expectation
-M = N  # num inducing point. exact (non-sparse) model is obtained by setting M=N
-shared_kernel = True  # shares the same kernel parameters across input dimension if true
 
-if M == N:
-    Z_init = tf.identity(X)  # X.copy()
-else:
-    Z_init = np.array([np.linspace(0, T, M) for i in range(D)]).T  # .reshape(M,1) # initial inducing variable locations
-
-max_iter = 2000
+################################
+#####  Generate GWP model  #####
+################################
 
 # Kernel
-if shared_kernel:
-    kernel = gpflow.kernels.SharedIndependent(
-        Sum([SquaredExponential() * Cosine(lengthscales=8. / (i + 1)) for i in range(3)]), output_dim=latent_dim)
-    kernel = gpflow.kernels.SharedIndependent(SquaredExponential(lengthscales=0.1),output_dim=latent_dim)
-else:
-    kern_list = [Sum([SquaredExponential() * Cosine(lengthscales=5. / (i + 1)) for i in range(2)]) for _ in range(latent_dim)]
-    kernel = gpflow.kernels.SeparateIndependent(kern_list)
+kernel = SquaredExponential(lengthscales=1.)
+#kernel = Sum([SquaredExponential() * Cosine(i+2) for i in range(2)])
 
-# Likelihood
-model_inverse = False
-additive_noise = True
+if shared_kernel:
+    kernel = SharedIndependent(kernel, output_dim=latent_dim)
+else:
+    kernel = gpflow.kernels.SeparateIndependent([kernel for _ in range(latent_dim)])
+# likelihood
 likelihood = WishartProcessLikelihood.FullWishartLikelihood(D, DoF, R=R,
                                                             additive_noise=additive_noise,
                                                             model_inverse=model_inverse)
-
-
-# Construct model
-Z = tf.identity(Z_init)  # Z_init.copy()
-iv = gpflow.inducing_variables.SharedIndependentInducingVariables(
-    gpflow.inducing_variables.InducingPoints(Z))  # multi output inducing variables
-
+# create gwp model
 wishart_process = WishartProcess.FullCovarianceWishartProcess(kernel, likelihood, D=D, DoF=DoF, inducing_variable=iv)
 if M == N:
     gpflow.set_trainable(wishart_process.inducing_variable, False)
 print_summary(wishart_process)
 
+#################################
+#####  Training & Inference #####
+#################################
 
-# train model
-optimizer = gpflow.optimizers.Scipy()
-optimizer.minimize(wishart_process.training_loss_closure(data),
-                   variables=wishart_process.trainable_variables,
-                   method="l-bfgs-b",
-                   options={"disp": True, "maxiter": max_iter},)
+
+
+## optimization parameters
+max_iter = ci_niter(200)
+learning_rate = 0.01
+minibatch_size = 25
+
+run_adam(wishart_process, data, max_iter, learning_rate, minibatch_size, plot=True)
 print_summary(wishart_process)
 
-# inspect resulting covariance matrix
+# obtain output covariance matrix. To do: make a function in GWP class for this.
+n_posterior_samples = 20000
 print(f"ELBO: {wishart_process.elbo(data):.3}")
-Sigma = wishart_process.mcmc_predict_density(X, Y, 100)
+Sigma = wishart_process.predict_mc(X, Y, n_posterior_samples)
+Sigma_map = wishart_process.predict_map(X)
 mean_Sigma = tf.reduce_mean(Sigma, axis=0)
+var_Sigma = tf.math.reduce_variance(Sigma, axis=0)
 
-# visualize latent functions for debugging
-mu, var = wishart_process.predict_f(X)
-fig, ax = plt.subplots(latent_dim,1, sharex=True, sharey=True, figsize=(10, 20))
-colors = ['darkred', 'firebrick', 'red', 'salmon']
-for i in range(latent_dim):
-    # ax[i].plot(X[:, i%4], Y[:, i%4], color='black', label='Observations')
-    # mean
-    ax[i].plot(X[:, i%4], mu[:, i], color=colors[i%4], label='Posterior mean')
-    ax[i].set_xlim((0, T))
+##############################
+#####  Visualize results #####
+##############################
 
-    # 2*std
-    top = mu[:, i] + 2.0 * var[:, i] ** 0.5
-    bot = mu[:, i] - 2.0 * var[:, i] ** 0.5
-    ax[i].fill_between(X[:, i%4], top, bot, alpha=0.3, color=colors[i%4], label='2$\sigma$')
-    ax[i].set_xlim((0, T))
-    if i == 2:
-        ax[i].set_ylabel('measurement')
-    if i == 3:
-        ax[i].set_xlabel('time')
+def plotMarginalCovariance(time, Sigma_mean, Sigma_map, Sigma_var, Sigma_gt, samples=None):
+    N, _, D = Sigma_gt.shape
+
+    f, axes = plt.subplots(nrows=D, ncols=D, figsize=(12, 12))
+    for i in range(D):
+        for j in range(D):
+            if i <= j:
+                axes[i, j].plot(time, Sigma_gt[:, i, j], label='Ground truth', color='C0')
+                axes[i, j].plot(time, Sigma_mean[:, i, j], label='VB', zorder=-5, color='red')
+                axes[i, j].plot(time, Sigma_map[:, i, j], label='map', zorder=-5, color='green')
+                # 2 standard deviations from the mean =\approx 95%
+                top = Sigma_mean[:, i, j] + 2.0 * Sigma_var[:, i, j] ** 0.5
+                bot = Sigma_mean[:, i, j] - 2.0 * Sigma_var[:, i, j] ** 0.5
+                # plot std -> to do
+                axes[i, j].fill_between(time[:,i], bot, top, color='red', alpha=0.05, zorder=-10, label='95% HDI')
+                if samples is not None:
+                    axes[i, j].plot(time, samples[:, i, j], label='function samples', zorder=-5, color='red', alpha=0.2)
+                if i == j:
+                    axes[i, j].set_title('Marginal variance {:d}'.format(i))
+                else:
+                    axes[i, j].set_title(r'Marginal covariance $\Sigma_{{{:d}{:d}}}$'.format(i, j))
+                axes[i, j].set_xlabel('Time')
+                if i == D - 1 and j == D - 1:
+                    axes[i, j].legend()
+            else:
+                axes[i, j].plot(time, Sigma_gt[:, i, j], label='Ground truth', color='C0')
+                axes[i, j].plot(time, Sigma_map[:, i, j], label='map', zorder=-5, color='green')
+#                axes[i, j].axis('off')
+
+    plt.subplots_adjust(top=0.9)
+    plt.suptitle('BANNER: Marginal $\Sigma(t)$', fontsize=14)
+
+plotMarginalCovariance(X, mean_Sigma, Sigma_map, var_Sigma, Sigma_gt, samples=None)
+
 plt.show()
-
-
-# Plot covariance matrix for first 10 timepoints
-time_points = np.linspace(0,M,3)
-for t, val in enumerate(time_points): #time_points:
-    fig, ax = plt.subplots(1,1)
-    ax.imshow(mean_Sigma[t], cmap='Reds')
-    ax.set_title(f'time {round(val)}')
-    plt.show()
-
-
